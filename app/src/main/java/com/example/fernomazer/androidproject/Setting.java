8583d3e17ec4f2a6fc4be82d2ca6ab303aeeb318
DESKTOP-A8I01GK\FernoMazer <Kittipan_panyapusit@cmu.ac.th>
2018-04-19 23:23:31 +0700

    Update setting page

---
+++ app/src/main/java/com/example/fernomazer/androidproject/Setting.java
@@ -0,0 +1,45 @@
+package com.example.fernomazer.androidproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Setting extends AppCompatActivity {
+
+
+    public void setLanguage(String selectlanguage) {
+        this.selectlanguage = selectlanguage;
+    }
+    private String selectlanguage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+        final String[] language = new String[]{"English","Australian","Chinese","Japanese","South Korean","Thai"};
+        Spinner spinnerLanguage = (Spinner) findViewById(R.id.spinnerLanguage);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_2, language);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLanguage.setAdapter(adapter);
+
+        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long j) {
+                Object itemLanguage = adapterView.getItemAtPosition(i);
+                selectlanguage = itemLanguage.toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+}
