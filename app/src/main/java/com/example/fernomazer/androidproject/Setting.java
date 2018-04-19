531d94baa90117936aea2b7e822c031dc0d288ac
DGNjameZ-PC\DGNjameZ <Kittipan_panyapusit@cmu.ac.th>
2018-04-20 03:59:04 +0700

    create save button in setting page

--- app/src/main/java/com/example/fernomazer/androidproject/Setting.java
+++ app/src/main/java/com/example/fernomazer/androidproject/Setting.java
@@ -1,15 +1,18 @@
 package com.example.fernomazer.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Setting extends AppCompatActivity {
     private String selectlanguage;
+    private Button saveBut;
 
     public String getSelectlanguage() {
         return selectlanguage;
@@ -19,15 +22,11 @@ public class Setting extends AppCompatActivity {
         this.selectlanguage = selectlanguage;
     }
 
-
-    protected TextView text1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        text1 = (TextView) findViewById(R.id.testText);
         Spinner dropdown = findViewById(R.id.spinnerLanguage);
         String[] language = new String[]{"English", "Thai", "China" , "Japan"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, language);
@@ -38,13 +37,21 @@ public class Setting extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Object item = parent.getSelectedItem();
                 setSelectlanguage(item.toString());
-                text1.setText(getSelectlanguage());
-
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        saveBut = (Button) findViewById(R.id.setSave);
+        saveBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Setting.this, MainActivity.class);
+                intent.putExtra("",getSelectlanguage());
+                startActivity(intent);
             }
         });
     }
